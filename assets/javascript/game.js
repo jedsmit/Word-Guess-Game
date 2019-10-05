5f286eafd7a9de4f0dd79e0cfabcc78d9cdb050b
Jed Smit <jedsmit1@gmail.com>
2019-10-05 00:13:25 -0700

    added stat counters, put repeated code into a function, added statements to compare guess with letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,92 @@
 //possible words
 var words = ["word", "words", "stuff", "things", "this", "that"];
-//
-var secretWord = generateWord(words);
-var underscore = ["_ "];
-var spaces = $("#secret-word");
-var usedLetters = $("#already-guessed")
+//variables
+var secretWord = "";
+var letterSpaces = $("#secret-word");
+var usedLetters = $("#already-guessed");
+var guessesLeft = $("#guesses-left");
+var userGuess = "";
+var guessArray = [];
+var numGuessLeft = 10;
+var usedArray = [];
+var wins = $("#wins");
+var losses = $("#losses");
+var numWins = 0;
+var numLosses = 0;
+var correctGuesses = 0;
+
 //randomly chooses word
 function generateWord(array) {
     return array[Math.floor(Math.random() * array.length)];
 };
-//counts letters in random word and makes underscores
 
-for (i = 0; i < secretWord.length - 1; i++) {
-    var blanks = "_ ";
-    underscore = underscore + blanks;
+//updates stats
+function updateStats() {
+    usedLetters.text("Already guessed: " + usedArray);
+    guessesLeft.text("Number of guesses left: " + numGuessLeft);
+    wins.text(numWins);
+    losses.text(numLosses);
+};
 
-}
-console.log(underscore);
-//displays underscores on page
+//-----------------------------------main game-------------------------------------
+document.onkeyup = function (event) {
+    secretWord = generateWord(words);
+    console.log(secretWord);
 
-//$(.text()
+    //counts letters in random word and makes underscores
+    for (i = 0; i < secretWord.length; i++) {
+        var blanks = "_";
+        guessArray.push(blanks);
+    }
+    //displays underscores on page 
+    letterSpaces.text((guessArray).join(" "));
+
+    //displays stats
+    updateStats();
+
+    //get letter guess from user
+    document.onkeyup = function (event) {
+        userGuess = event.key.toLowerCase();
+        console.log(userGuess);
+        numGuessLeft--;
+        updateStats();
+        //compare user guess to each letter in the secret word
+        //if matched replace underscore with letter, 
+        if (secretWord.includes(userGuess)) {
+            var index = secretWord.indexOf(userGuess);
+            guessArray[index] = userGuess;
+            letterSpaces.text((guessArray).join(" "));
+            correctGuesses++;
+            updateStats();
+            if (correctGuesses === secretWord.length) {
+                wins++;
+                updateStats();
+                alert("You won!s Want to try again?");
+            }
+        } else {
+            //if no match, decrement guesses left, add letter to used array
+            usedArray.push(userGuess);
+            usedLetters.text("Already guessed: " + usedArray.join(" "));
+            if (numGuessLeft === 0) {
+                numLosses++;
+                updateStats();
+                alert("You lost. Want to try again?");
+            }
+
+        }
+
+    };
+};
 
 
 
 
 
-console.log(secretWord.length);
+
+
+
+
+
+
+
+console.log(secretWord.length)
